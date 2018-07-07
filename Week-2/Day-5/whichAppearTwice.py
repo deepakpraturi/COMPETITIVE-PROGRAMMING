80f01343bf342cc076f87facd32a7d02b590694a
Deepak Praturi <deepu11496@gmail.com>
2018-07-07 09:42:46 +0530

    week-2 day-5

---
+++ Week-2/Day-5/whichAppearTwice.py
@@ -0,0 +1,55 @@
+import unittest
+
+
+def find_repeat(numbers_list):
+
+    # Find the number that appears twice
+    dict={}
+    for x in numbers_list:
+        if x in dict:
+            dict[x]+=1
+        else:
+            dict[x]=1
+    print(dict)
+    for x in dict:
+        if(dict[x]==2):
+            return x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_short_list(self):
+        actual = find_repeat([1, 2, 1])
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_medium_list(self):
+        actual = find_repeat([4, 1, 3, 4, 2])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+    def test_long_list(self):
+        actual = find_repeat([1, 5, 9, 7, 2, 6, 3, 8, 2, 4])
+        expected = 2
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
